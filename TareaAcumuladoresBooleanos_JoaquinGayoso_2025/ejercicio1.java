6ade174df3761d44deef7e075d4acbe326e28343
Joacco761 <joaquin@acg-sa.com.ar>
2025-08-20 16:59:16 -0300

    without time

--- TareaAcumuladoresBooleanos_JoaquinGayoso_2025/ejercicio1.java
+++ TareaAcumuladoresBooleanos_JoaquinGayoso_2025/ejercicio1.java
@@ -1,22 +1,22 @@
 
 public class ejercicio1 {
 	
-	/*public boolean estaEnLaFila(int[] fila, int n) {
+	public boolean estaEnLaFila(int[] fila, int n) {
 		for(int i=0; i<fila.length; i++) {
 			if(fila[i]==n) {
 				return true;
 			}
 		}
 		return flase;
-	}*/
+	}
 	
 	//verifica si hay alguna fila con sus elementos multiplos del numero recibido por parametro
 	public boolean todosMultiplosEnAlgunaFila(int[][] mat, int num) {
 		if (mat.length==0 || num<0) {
 			return false;
 		}
-		boolean ret1=false;   /*variable para comprobar si hay alguna fila que cumpla la comndicion*/
-		boolean ret2=true;   /*variabloe para com´probar que todos los elementops sean multiplos*/
+		boolean ret1=false;   /*variable para comprobar si hay alguna fila que cumpla la condicion*/
+		boolean ret2=true;   /*variable para comprobar que todos los elementos sean multiplos*/
 		for(int i=0; i<mat.length;i++) {
 			for(int j=0;j<mat[i]; j++) {
 				ret2 =ret2 && (mat[i][j]%num == 0);
@@ -27,5 +27,11 @@ public class ejercicio1 {
 		
 	}
 	
-
+	//verifica, fila a fila, que haya interseccion entre las filas de las matrices recibidas como parametro
+	public boolean hayInterseccionPorFila(int[][] mat1, int[][]mat2) {
+		if(mat1.length==0 || mat2.length==0 || mat1.length!=mat2.length) {
+			return false;
+		}
+		
+	}
 }
